607d029345ee22cb1f20ce29cb47da099ae40e31
LukeMattingly <mattingly.luke@yahoo.comm>
2023-08-06 18:42:12 -0600

    box smart pointers

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -16,11 +16,13 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use crate::List::Nil;
+use crate::List::Cons;
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +35,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    let empty_list = Box::new(Nil);
+    *empty_list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    list
 }
 
 #[cfg(test)]
